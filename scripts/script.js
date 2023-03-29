ae56151ea862ff7e6564f1498df9c9b2893e77a1
Darien <dlowe39@my.bcit.ca>
2023-03-29 14:09:05 -0700

    button toggle thing"

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,30 @@
 //get ref rto button
-const but = document.querySelector("#clickMe")
+const but = document.querySelector("#clickMe");
 //define func to alert
-function aler(){
-    alert("alert");
+function aler() {
+  alert("alert");
+  //but.removeEventListener("click", aler);
 }
 //add func as event list
-but.addEventListener("click", aler);
+but.addEventListener("click", aler, {once:true});//aler now only triggers once.
+
+
+//------------------------------------------------------------------------------------
+
+function changeBGPink(){
+    document.body.style.background="pink"
+}
+
+but.addEventListener("click", changeBGPink);
+
+function changeText(){
+    if (but.textContent == "Click"){
+        but.textContent ="Clicked" 
+    } else{
+        but.textContent="Click"
+    }
+    //console.log(but.textContent=="Click")
+}
+
+but.addEventListener("click", changeText);
+
